c1570e0e9f83fad327d0cf5b6db3d0d2f308d44f
Kauã Morais <kaua.morais22@escola.pr.gov.br>
2024-06-24 11:44:23 -0300

    ok

---
+++ index.js
@@ -0,0 +1,11 @@
+let resposta;
+let anagrama= [];
+
+console.log('Digite uma palavra');
+process.stdin.on('data', function (data){
+    let resposta = data.toString().trim();
+    for(let i = 0; i < resposta.lentgh; i++){
+        let aux = resposta
+        
+    }
+})
